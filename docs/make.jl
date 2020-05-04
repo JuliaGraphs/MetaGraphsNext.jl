1d0e9e27bf21e07fe3ac7f2003ebf96ad0f4aa1c
Brandon Taylor <brandon.taylor221@gmail.com>
2020-05-04 16:55:06 -0400

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using MetaGraphsNext
 using Documenter: deploydocs, makedocs
+
 makedocs(sitename = "MetaGraphsNext.jl", modules = [MetaGraphsNext], doctest = false)
 deploydocs(repo = "github.com/JuliaGraphs/MetaGraphsNext.jl.git")

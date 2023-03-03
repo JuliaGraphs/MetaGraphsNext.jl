b869a573adeab553f5253cde8f5cc9a523aa0b45
Thomas Christensen <tchr@mit.edu>
2023-03-03 09:48:26 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,4 +30,21 @@ using Test
             include(joinpath("tutorial", "4_type_stability.jl"))
         end
     end
+
+    @testset verbose = true "Short-form add_vertex!/add_edge!" begin
+        # short-form
+        mg = MetaGraph(Graph(); Label=Symbol, VertexData=Nothing, EdgeData=Nothing)
+        @test add_vertex!(mg, :A)
+        @test add_vertex!(mg, :B)
+        @test add_edge!(mg, :A, :B)
+
+        # long-form
+        mg′ = MetaGraph(Graph(); Label=Symbol, VertexData=Nothing, EdgeData=Nothing)
+        @test add_vertex!(mg′, :A, nothing)
+        @test add_vertex!(mg′, :B, nothing)
+        @test add_edge!(mg′, :A, :B, nothing)
+
+        # compare
+        @test mg == mg′
+    end
 end

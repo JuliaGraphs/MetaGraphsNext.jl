93a3a081342a68658d2dd33da7ca622fdcc56fa2
Koki Fushimi <paalon1936@gmail.com>
2023-05-30 09:55:38 +0900

    Add and fix tests.

--- test/tutorial/3_files.jl
+++ test/tutorial/3_files.jl
@@ -35,7 +35,7 @@ simple_str = mktemp() do file, io
 end
 
 print(simple_str)  #md
-@test simple_str == "graph T {\n    a\n    b\n    a -- b\n}\n"  #src
+@test simple_str == "graph T {\n    \"a\"\n    \"b\"\n    \"a\" -- \"b\"\n}\n"  #src
 
 #-
 
@@ -60,4 +60,32 @@ end
 
 print(complicated_str)  #md
 @test complicated_str ==  #src
-    "digraph G {\n    tagged = true\n    a [code_1 = 1, code_2 = 2]\n    b [code = 2]\n    a -> b [code = 12]\n}\n"  #src
+    "digraph G {\n    tagged = true\n    \"a\" [code_1 = 1, code_2 = 2]\n    \"b\" [code = 2]\n    \"a\" -> \"b\" [code = 12]\n}\n"  #src
+
+with_spaces = MetaGraph(
+    DiGraph();
+    label_type=String,
+    vertex_data_type=Dict{Symbol,String},
+    edge_data_type=Dict{Symbol,String},
+)
+
+with_spaces["a b"] = Dict(:label => "A B")
+
+with_spaces["c d"] = Dict(:label => "C D")
+
+with_spaces["a b", "c d"] = Dict(:label => "A B to C D")
+
+with_spaces_str = mktemp() do file, io
+    savegraph(file, with_spaces, DOTFormat())
+    read(file, String)
+end
+
+print(with_spaces_str)
+@test with_spaces_str ==
+"""
+digraph G {
+    "a b" [label = "A B"]
+    "c d" [label = "C D"]
+    "a b" -> "c d" [label = "A B to C D"]
+}
+"""

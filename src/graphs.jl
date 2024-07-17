02d33b8ec7042888a65f6e12d31e5f75602cfad9
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-07-17 13:00:20 +0200

    Clarify docstring

--- src/graphs.jl
+++ src/graphs.jl
@@ -189,7 +189,8 @@ Add an edge `(label_1, label_2)` to MetaGraph `meta_graph` with metadata `data`.
 If the `EdgeData` type of `meta_graph` is `Nothing`, `data` can be omitted.
 
 Return `true` if the edge has been added, `false` otherwise.
-If `(label_1, label_2)` already existed, its data is updated to `data` and `false` is returned nonetheless.
+If one of the labels does not exist, nothing happens and `false` is returned (the label is not inserted).
+If `(label_1, label_2)` already exists, its data is updated to `data` and `false` is returned nonetheless.
 """
 function Graphs.add_edge!(meta_graph::MetaGraph, label_1, label_2, data)
     if !haskey(meta_graph, label_1) || !haskey(meta_graph, label_2)
